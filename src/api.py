82a89dba75f604b05722970e7454154ef2905910
Ashley Newman <ashznewman@gmail.com>
2018-01-26 11:33:19 -0800

    Adding API class

---
+++ src/api.py
@@ -0,0 +1,28 @@
+# =============================================================================
+# Created by asnewman (Ashley Newman)
+# =============================================================================
+
+import requests
+
+# Class for executing API calls to Robinhood or to the test API
+class API:
+    baseUrl = 'https://api.robinhood.com'
+    urls = {
+        'login': baseUrl + '/api-token-auth/username',
+        'quote': baseUrl + '/quotes/',
+        'account': baseUrl + '/accounts/',
+        'order': baseUrl + '/orders/',
+        'instrument': baseUrl + '/instruments/'
+    }
+
+    # Constructor that takes in whether the API class will be setup for testing
+    # or not.
+    def __init__(self, test=False):
+        if test == True:
+            self.baseUrl = 'http://localhost:5005/'
+
+    def login(self, username, password):
+        return requests.post(self.urls['login'], data = {
+                'username': username,
+                'password': password
+            }
